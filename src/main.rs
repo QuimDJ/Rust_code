9dc0cd9e7470b2940ef7a2981c7863beced90d12
esbrinachain <ebrinachain@email.com>
2025-12-09 11:23:04 +0100

    C4-M3-comm-02

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,29 @@
 fn main() {
 
     let v=vec![1,2,3,4];
-    let my_iterator=v.into_iter();    
-    for m in my_iterator { println!("{m:?}")};
+    let my_iterator=v.into_iter();
+    println!("{my_iterator:?}");
+    for m in my_iterator { println!("{m}");};
+    // hemos consumido el vector con la iteración println!("{v:?}"); da borrow
+
+    let v=vec![1,2,3,4];
+    let my_iterator=v.iter();
+    println!("{my_iterator:?}");
+    for m in my_iterator { println!("{m}");};
+    println!("{v:?}"); // con iter() no consumimos el vector pero es immutable
+
+    let mut v=vec![1,2,3,4];
+    let my_iterator=v.iter_mut();
+    println!("{my_iterator:?}");
+    for m in my_iterator { 
+        *m=5; 
+        println!("{m}"); 
+    };
+    println!("{v:?}"); // iteramos y ademas modificamos usando deferenciación
+
+    // into_iter() consume el vector.
+    // iter() es referencia immutable, no consume el vector.
+    // iter_mut() es referencia mutable, permite modificar el vector.
 
 }
 
